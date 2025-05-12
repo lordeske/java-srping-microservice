f6f8a6387e68995e5a37785281f975acc32424bb
lordeske8 <eskicmihajlo55@gmail.com>
2025-05-12 23:18:38 +0200

    api gateway, keycloack

--- Booking System/order-service/src/main/java/com/order_service/services/OrderService.java
+++ Booking System/order-service/src/main/java/com/order_service/services/OrderService.java
@@ -7,7 +7,6 @@ import com.order_service.event.BookingEvent;
 import com.order_service.repository.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
